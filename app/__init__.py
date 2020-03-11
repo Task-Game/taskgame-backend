b1fe39c986cd8434ea252cd6400f148b607e51c6
JoaoMWatson <joaopedrowatson@hotmail.com>
2020-03-11 16:30:53 -0300

    JESUS CRISTO FUNCIONOU EU N ACREDITO AAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 from config import BaseConfig
 from config import configure_app
 
-from api.Usuario import usuario_routes
+from .api.Usuario import usuario_routes
 
 def create_app():
     app = Flask(__name__)
