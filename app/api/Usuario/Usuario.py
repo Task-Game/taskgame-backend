4ebfbbf8ccf0dd16ab7ed5678d24dfc87d98988a
JoaoMWatson <joaopedrowatson@hotmail.com>
2020-03-13 17:23:24 -0300

    rest routes criadas para usuario

--- app/api/Usuario/Usuario.py
+++ app/api/Usuario/Usuario.py
@@ -1,7 +1,17 @@
+import json
+from flask import Response, make_response
+# import models
 class Usuario():
     def __init__(self):
         pass
 
 
     def authenticated_login(email, senha):
+        if email == 'teste' and senha == 'teste':
+            response  = {'Aproves':True}
+            return make_response(json.dumps(response)), 200
+
+    def index():
         pass
+
+            
--- app/api/Usuario/usuario_routes.py
+++ app/api/Usuario/usuario_routes.py
@@ -4,6 +4,69 @@ import json
 user_bp = Blueprint('user_bp', __name__)
 
 
+@user_bp.route('/api/v1.0/user/', methods=['GET'])
+def index():
+    response = Usuario.index()
+
+    return response
+
+
+@user_bp.route('/api/v1.0/user/new', methods=['GET'])
+def new():
+    pass
+
+
+@user_bp.route('/api/v1.0/user/', methods=['POST'])
+def create():
+    response = Usuario.create()
+
+    return response
+
+@user_bp.route('/api/v1.0/user/<:idUser>', methods=['GET'])
+def show(idUser):
+    request_json = request.get_json()
+
+    idUser = request_json['data']['idUser']
+
+    user = Usuario.show(idUser)
+
+    return user
+
+
+@user_bp.route('/api/v1.0/user/<:idUser>/edit', methods=['GET'])
+def edit(idUSer):
+    request_json = request.get_json()
+
+    idUser = request_json['data']['idUser']
+    # get another request body data here
+
+    user = Usuario.edit(idUser)
+
+    return user
+
+@user_bp.route('/api/v1.0/user/<:idUser>', methods=['PUT'])
+def upgrade(idUser):
+    request_json = request.get_json()
+
+    idUser = request_json['data']['idUser']
+    # get another request body data here
+
+    user = Usuario.upgrade(idUser)
+
+    return user
+
+
+@user_bp.route('/api/v1.0/user/<:idUser>', methods=['DELETE'])
+def destroy(idUser):
+    request_json = request.get_json()
+
+    idUser - request_json['data']['idUser']
+
+    user = Usuario.destroy(idUser)
+
+    return user
+
+
 @user_bp.route('/api/v1/user/login', methods=['GET'])
 def user_login():
     request_json = request.get_json()
@@ -14,3 +77,4 @@ def user_login():
     loggin_aprove = Usuario.authenticated_login(email, senha)
 
     return loggin_aprove
+
