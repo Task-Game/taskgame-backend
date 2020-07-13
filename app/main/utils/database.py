fdc9d1f865d465319e4b01d27612c61e0fed15ef
JoaoMWatson <joaopedrowatson@hotmail.com>
2020-07-13 17:44:21 -0300

    pequenas alteracoes

--- app/main/utils/database.py
+++ app/main/utils/database.py
@@ -15,8 +15,6 @@ class FrequenciaTable(db.Model):
 
     # A chave desta tabela esta na que se refere
     tarefa = db.relationship('Tarefa', backref="frequencia")
-
-
 class ChecklistTable(db.Model):
     __tablename__ = 'Checklist'
 
@@ -35,8 +33,6 @@ class ChecklistTable(db.Model):
     feito = db.Column(db.Boolean,
                       unique=False,
                       nullable=False)
-
-
 class RaridadeTable(db.Model):
     __tablename__ = 'Raridade'
 
@@ -180,51 +176,49 @@ class TarefaTable(db.Model):
 
 
 class UsuarioTable(db.Model):
-    """
-    Classe model responsavel pelos dados do usuario
-    """
-
-    __tablename__ = 'Usuario'
-
-    idUsuario = db.Column(db.Integer,
-                          primary_key=True)
-
-    nome = db.Column(db.String(80),
-                     unique=False,
-                     nullable=False)
-
-    email = db.Column(db.String(60),
-                      unique=True,
-                      nullable=False)
-
-    cargo = db.Column(db.String(50),
-                        unique=False,
-                        nullable=True)
-
-    dataCriacao = db.Column(db.DateTime,
-                            unique=False,
-                            nullable=False,
-                            default=datetime.datetime.now())
-
-    codigoConfirmacao = db.Column(db.String(50),
-                                  unique=False,
-                                  nullable=False)
-
-    senha = db.Column(db.String(250),
+     """
+     Classe model responsavel pelos dados do usuario
+     """
+ 
+     __tablename__ = 'Usuario'
+ 
+     idUsuario = db.Column(db.Integer,
+                           primary_key=True)
+ 
+     nome = db.Column(db.String(80),
                       unique=False,
                       nullable=False)
-
-    credito = db.Column(db.Integer,
-                        unique=False,
-                        nullable=True,)
-
-    usuario_grupo = db.relationship(
-        'usuario_grupo', backref="usuario")
-
-    usuario_tarefa = db.relationship(
-        'usuario_tarefa', backref="usuario")
-
-
+ 
+     email = db.Column(db.String(60),
+                       unique=True,
+                       nullable=False)
+ 
+     cargo = db.Column(db.String(50),
+                         unique=False,
+                         nullable=True)
+ 
+     dataCriacao = db.Column(db.DateTime,
+                             unique=False,
+                             nullable=False,
+                             default=datetime.datetime.now())
+ 
+     codigoConfirmacao = db.Column(db.String(50),
+                                   unique=False,
+                                   nullable=False)
+ 
+     senha = db.Column(db.String(250),
+                       unique=False,
+                       nullable=False)
+ 
+     credito = db.Column(db.Integer,
+                         unique=False,
+                         nullable=True,)
+ 
+     usuario_grupo = db.relationship(
+         'usuario_grupo', backref="usuario")
+ 
+     usuario_tarefa = db.relationship(
+         'usuario_tarefa', backref="usuario")
 usuario_grupo = db.Table('usuario_grupo',
                          db.Column('idUsuarioGrupo', db.Integer,
                                    primary_key=True),
@@ -235,7 +229,6 @@ usuario_grupo = db.Table('usuario_grupo',
                                    db.ForeignKey('Grupo.idGrupo'))
 
                          )
-
 usuario_tarefa = db.Table('usuario_tarefa',
                           db.Column('idUsuarioTarefa', db.Integer,
                                     primary_key=True),
